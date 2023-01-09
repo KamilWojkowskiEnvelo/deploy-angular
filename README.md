85e72a3a545f493181f126ebbbcd0df84d34407e
KamilWojkowskiEnvelo <kamil.wojkowski@ppuc.pl>
2023-01-09 08:02:23 +0100

    feat: to netlify test

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# deploy-angular
+### How to run app
+
+1. install dependencies `npm ci`
+2. add `.env` file to root folder
+3. start app `npm start`
+
+Sample `.env` file
+
+```
+API_URL=http://rickandmortyapi.com/api
+PRODUCTION=false
+```
---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { API_URL, IS_PRODUCTION } from '@core/env.token';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    application shell | {{ envs.url }} | {{ envs.prod }}
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {
+  envs = {
+    url: inject(API_URL),
+    prod: inject(IS_PRODUCTION),
+  };
+}
